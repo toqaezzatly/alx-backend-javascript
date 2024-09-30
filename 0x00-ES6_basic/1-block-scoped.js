4fb03683f5c04e4ce8022bdcb8dc50dbea6adba2
toqaezzatly <toqaezzatly@gmail.com>
2024-09-30 15:26:13 -0700

    ...spread1 ...s...spread

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Changed var to let
-  let task2 = true; // Changed var to let
+  const task = false; // Changed var to let
+  const task2 = true; // Changed var to let
 
   if (trueOrFalse) {
-    let task = true; // This task is block-scoped and does not overwrite the outer task
-    let task2 = false; // This task2 is block-scoped and does not overwrite the outer task2
+    const task = true; // This task is block-scoped and does not overwrite the outer task
+    const task2 = false; // This task2 is block-scoped and does not overwrite the outer task2
   }
 
   return [task, task2]; // Returns the outer task and task2
