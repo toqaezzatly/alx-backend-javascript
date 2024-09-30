9a52ea2e35eece17c75c1e6f9dd173f8d5cf3a1a
toqaezzatly <toqaezzatly@gmail.com>
2024-09-30 15:19:20 -0700

    args.lengths

---
+++ 0x00-ES6_basic/4-main.js
@@ -0,0 +1,5 @@
+import returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one"));                         // Should print 1
+console.log(returnHowManyArguments("one", "two", 3, "4th"));       // Should print 4
+
