4b09b8b15d616fa0175ddb283de5055802caa473
toqaezzatly <toqaezzatly@gmail.com>
2024-09-30 15:55:22 -0700

    if key is the ve value keep the key

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,  // Using shorthand property name
+    gdp,     // Using shorthand property name
+    capita   // Using shorthand property name
+  };
+
+  return budget;
+}
