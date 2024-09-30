d89204c52e230fde1acf6e7c5a5532569249f83b
toqaezzatly <toqaezzatly@gmail.com>
2024-09-30 16:01:57 -0700

    check7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    income,  // Using shorthand property name
-    gdp,     // Using shorthand property name
-    capita   // Using shorthand property name
+    income,
+    gdp,
+    capita,
   };
 
   return budget;
