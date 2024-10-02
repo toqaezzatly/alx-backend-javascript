144df089a040666845d6ddd2ab2015eece748d85
toqaezzatly <toqaezzatly@gmail.com>
2024-10-02 10:27:11 -0700

    linters

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -6,10 +6,9 @@ console.log(getFullResponseFromAPI(false));
 
 // To observe the output of the resolved or rejected promise, use async/await or `.then` and `.catch`
 getFullResponseFromAPI(true)
-  .then(response => console.log(response))
-  .catch(error => console.error(error));
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
 
 getFullResponseFromAPI(false)
-  .then(response => console.log(response))
-  .catch(error => console.error(error));
-
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
