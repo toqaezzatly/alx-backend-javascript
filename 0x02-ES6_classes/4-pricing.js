61b511edd5644d6ef87818c0d1464e78069e5a54
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-03 19:27:21 +0000

    static

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+import Currency from "./3-currency";
+
+export default class Pricing{
+    constructor(amount, currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
+    get amount(){
+        return this._amount;
+    }
+
+    set amount(value){
+        if (typeof value !== 'number'){
+            throw new TypeError('Amount must be a number');
+        }
+        this._amount = value;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+    
+    set currency(value){
+        if (!(value instanceof Currency)){
+            throw new TypeError('Currency must be a Currency instance');
+        }
+        this._currency = value;
+    }
+    
+    displayFullPrice() {
+        return `${this.amount} ${this.currency.displayFullCurrency()}`;
+      }
+    
+      static convertPrice(amount, conversionRate) {
+        if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+          throw new TypeError('Both amount and conversion rate must be numbers');
+        }
+        return amount * conversionRate;
+      }
+    
+}
