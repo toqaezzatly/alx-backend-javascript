7000887ca59dc1102d5af7239ec3a84aef86ad54
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-03 19:04:33 +0000

    why static ?

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+    constructor(code, name) {
+      this.code = code;
+      this.name = name;
+    }
+  
+    // Getter for _code
+    get code() {
+      return this._code;
+    }
+  
+    // Setter for _code with type validation
+    set code(value) {
+      if (typeof value !== 'string') {
+        throw new TypeError('Code must be a string');
+      }
+      this._code = value;
+    }
+  
+    // Getter for _name
+    get name() {
+      return this._name;
+    }
+  
+    // Setter for _name with type validation
+    set name(value) {
+      if (typeof value !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = value;
+    }
+  
+    // Method to display full currency
+    displayFullCurrency() {
+      return `${this.name} (${this.code})`;
+    }
+  }
+  
