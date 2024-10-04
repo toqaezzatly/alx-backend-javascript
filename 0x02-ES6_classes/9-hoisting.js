a99344aa4ed122b9609bf6390d6197bf21285521
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-04 00:55:26 +0000

    check changes

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-// HolbertonClass.js
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -14,16 +13,11 @@ export class HolbertonClass {
   }
 }
 
-// Create instances of HolbertonClass
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-// StudentHolberton.js
 export class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) { // Corrected parameter name
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass; // Use the parameter
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -31,20 +25,22 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass; // Corrected to access the instance property
+    return this.holbertonClass;
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`; // Corrected 'self' to 'this'
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-// Create instances of StudentHolberton
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// List of students
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
