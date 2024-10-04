f407b25557e12c3b1dbfdf494533e67de5561355
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-04 01:16:15 +0000

    still did not get it

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,11 +2,16 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
+    super(brand, motor, color); // Call the parent constructor
+    this._range = range; // Store the range attribute
   }
 
+  get range() {
+    return this._range; // Getter for range
+  }
+
+  // Override cloneCar method to return an instance of Car
   cloneCar() {
-    return new Car();
+    return new Car(this._brand, this._motor, this._color);
   }
 }
