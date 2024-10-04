68f3d99be8fc08c0b1a7e62c9eeac157d067a8f3
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-04 00:18:45 +0000

    I am the Abstract one can not be used  but it is my rules

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    if (!this.Building && !this.evacuationWarningMessage) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly.');
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this.sqft = sqft;
   }
 
+  // Getter for _sqft
   get sqft() {
     return this._sqft;
   }
 
+  // Setter for _sqft with type validation
   set sqft(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Square feet must be a number');
