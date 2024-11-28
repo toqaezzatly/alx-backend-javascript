540c52499a4e2a74f377226fb18d9d7368d5d8d6
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-11-28 16:01:22 +0200

    k

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,33 +1,11 @@
 #!/usr/bin/env node
 
-// Import necessary modules
-const readline = require('readline');
-
-// Create an interface for user input
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// Prompt user for their name and handle their input
-const askName = () => {
-  return new Promise((resolve) => {
-    rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-      resolve(name);
-    });
-  });
-};
-
-// Main function
-const main = async () => {
-  try {
-    const name = await askName(); // Await user input
-    console.log(`Your name is: ${name}`);
-  } finally {
-    console.log('This important software is now closing');
-    rl.close(); // Close the readline interface
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write('This important software is now closing\n');
   }
-};
-
-// Run the main function
-main();
+});
