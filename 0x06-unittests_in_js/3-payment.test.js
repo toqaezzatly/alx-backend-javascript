12c98c4dc51a4c9a5d7882f1454ffaf80751e5ec
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-11-28 19:06:48 +0200

    spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const sinon = require('sinon');
+const assert = require('assert');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+    it('should call Utils.calculateNumber with correct arguments', () => {
+        const spy = sinon.spy(Utils, 'calculateNumber');
+
+        sendPaymentRequestToApi(100, 20);
+
+        assert.strictEqual(spy.calledOnceWithExactly('SUM', 100, 20), true);
+
+        spy.restore();
+    });
+});
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const Utils = {
+    calculateNumber: function(type, a, b){
+        a = Math.round(a);
+        b = Math.round(b);
+
+
+        if (type === 'SUM'){
+            return a + b;
+        }else if (type === 'SUBTRACT'){
+            return a - b; 
+        }else if (type === 'DIVIDE') {
+            if (b === 0) return 'Error';
+            return a / b;
+        }
+    }
+}
+module.exports = Utils;
