bb9b0a2b90ed36187ba36e9740cab3213b09e22c
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-11-29 00:35:54 +0200

    hook

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    // Create a spy before each test
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    // Restore the spy after each test
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" when called with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('should log "The total is: 20" when called with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
